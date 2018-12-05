dfcd1d7583c367edb0206c907f28cf83e712e577
Grzegorz Wojciechowski <gwojciech@newterm.pl>
2018-12-05 20:51:47 +0100

    Daemon
    *addded first daemon collecting temperature from warsaw

---
+++ Daemons/get_temperature.py
@@ -0,0 +1,36 @@
+from weather import Weather, Unit
+import time
+import sys
+import socket
+
+class TemperatureReader:
+
+	def send_to_server(self, value):
+		timestamp = int(time.time())
+		message = '{},{}'.format(timestamp, value)
+		client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		client.connect(('localhost', 14572))
+		client.send(message)
+
+		# check proper response
+		response = client.recv(4096)
+		if (message != response):
+			print("Send failed!")
+
+
+	def update_values(self):
+		weather = Weather(unit=Unit.CELSIUS)
+		lookup = weather.lookup_by_location('warsaw')
+		condition = lookup.condition
+		self.send_to_server(int(condition.temp))
+
+
+def main(argv=None):
+	temp_reader = TemperatureReader()
+
+	while(True):
+		temp_reader.update_values()
+		time.sleep(10)
+
+if __name__ == "__main__":
+	sys.exit(main())
