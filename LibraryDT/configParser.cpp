71674f853cbb7fa5c7220e90418c6bb3c3500b30
Grzegorz Wojciechowski <gwojciech@newterm.pl>
2018-12-05 21:38:55 +0100

    Config
    * added example config file based on XML
    * added file with parser class

---
+++ LibraryDT/configParser.cpp
@@ -0,0 +1,17 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  configParser.cpp
+ *
+ *    Description:  Program that parses config.xml, gets info about what daemons are needed
+ *					to start and crating tables for non existing yet parameters
+ *
+ *         Author:  Grzegorz Wojciechowski, Michał Skarzyński
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
+class ConfigParser {
+	// here will be implementation using rapidxml library
+};
